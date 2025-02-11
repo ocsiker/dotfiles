a4f60d98192ed8944341c1ba594a9bc509cfe9bb
ocsiker <qthang89vn@gmail.com>
2025-02-11 12:28:29 +0700

    chores:backup stabled

--- importBackup.sh
+++ importBackup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -ra --update --stats --progress --exclude={'.config/google-chrome','.cargo','.cache','.npm','.gnupg','.local','.ssh','.m2','.mozilla'} ~/Alpha/backupDotfiles/ .
+rsync -ra --update --stats --progress --exclude={'.config/google-chrome','.cargo','.cache','.npm','.gnupg','.local','.ssh','.m2','.mozilla','.codeium'} ~/Alpha/backupDotfiles/ .
