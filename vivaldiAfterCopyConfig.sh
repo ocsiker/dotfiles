324436f95487e89f9ff4ba7658d765bb8df9ea56
ocsiker <qthang89vn@gmail.com>
2025-12-05 04:52:40 +0000

    feat: xoa file khong can thiet de load duoc config vivaldi sau khi saochep

---
+++ vivaldiAfterCopyConfig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# xoa nhung file khong can thiet de chay duoc config cua vivaldi
+rm -rf .config/vivaldi/Singleton* .config/vivaldi/Default/GPUCache .config/vivaldi/ShaderCache .config/vivaldi/Default/Login\ Data .config/vivaldi/Default/Login\ Data-journal .config/vivaldi/Default/{Cookies,Cookies-journal}
+
