361c39c003dc4697878ae527e527dc9a26dac05b
ocsiker <qthang89vn@gmail.com>
2025-12-06 02:47:19 +0700

    feat(sh): install git_completion

---
+++ git_completion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#cài đặt git-completion
+echo "INSTALL git-completion"
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
