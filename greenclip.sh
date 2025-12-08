0a18a42195f150320e83252e70a804048f3be2d1
ocsiker <qthang89vn@gmail.com>
2025-12-08 11:24:41 +0700

    Revert "chores: ordered list"
    This reverts commit 48f2fd39226584327fadf4e710fbfc9eaf1558b5.

--- greenclip.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /tmp/greenclip
-cd /tmp/greenclip
-wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip
-
-chmod +x greenclip
-
-sudo mv greenclip /usr/local/bin/
