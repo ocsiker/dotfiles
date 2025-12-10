d1baeb4321417b136f1b34a9110ea130c273cf49
ocsiker <qthang89vn@gmail.com>
2025-12-10 10:57:37 +0000

    fix(vivaldi): path

--- vivaldiAfterCopyConfig.sh
+++ vivaldiAfterCopyConfig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # xoa nhung file khong can thiet de chay duoc config cua vivaldi
+cd $HOME
 rm -rf .config/vivaldi/Singleton* .config/vivaldi/Default/GPUCache .config/vivaldi/ShaderCache .config/vivaldi/Default/Login\ Data .config/vivaldi/Default/Login\ Data-journal .config/vivaldi/Default/{Cookies,Cookies-journal}
 
-bash ./preference.sh
+bash $HOME/dotfiles/preference.sh
