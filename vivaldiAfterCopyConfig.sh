3dcba7417b3da912cc4f6e84c027e9b9252acddc
ocsiker <qthang89vn@gmail.com>
2025-12-10 10:52:30 +0000

    feat(vivaldi): run backup preferences

--- vivaldiAfterCopyConfig.sh
+++ vivaldiAfterCopyConfig.sh
@@ -3,3 +3,4 @@
 # xoa nhung file khong can thiet de chay duoc config cua vivaldi
 rm -rf .config/vivaldi/Singleton* .config/vivaldi/Default/GPUCache .config/vivaldi/ShaderCache .config/vivaldi/Default/Login\ Data .config/vivaldi/Default/Login\ Data-journal .config/vivaldi/Default/{Cookies,Cookies-journal}
 
+bash ./preference.sh
