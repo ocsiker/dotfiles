cb7e463676348a40fec9d941cbb606d2d0de3195
ocsiker <qthang89vn@gmail.com>
2025-12-18 08:58:16 +0700

    feat(nvim): resign key for vim-abolish

---
+++ home/nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        -- Khai báo: cụm phím "cr" có tên là "Coerce (Abolish)"
+        { "cr", group = "Coerce (Abolish)" },
+      },
+    },
+  },
+}
