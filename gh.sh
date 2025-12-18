89490aa6d1a534d97c95d1bd95da3e371ef53047
ocsiker <qthang89vn@gmail.com>
2025-12-18 13:45:30 +0700

    feat(github): add script install github

---
+++ gh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# 1. Tạo thư mục keyring nếu chưa có
+sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+	sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+
+# 2. Thêm repo của GitHub vào danh sách nguồn
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
+# 3. Cập nhật và cài đặt
+sudo apt update
+sudo apt install gh -y
