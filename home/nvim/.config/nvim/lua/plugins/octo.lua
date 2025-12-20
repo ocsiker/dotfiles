209ae4672f2975c9e19e19473ea7b09baf8eb9d6
ocsiker <qthang89vn@gmail.com>
2025-12-21 06:06:22 +0700

    feat(nvim): octo using with telescope

--- home/nvim/.config/nvim/lua/plugins/octo.lua
+++ home/nvim/.config/nvim/lua/plugins/octo.lua
@@ -1,29 +1,46 @@
 return {
   "pwntester/octo.nvim",
-  opts = function(_, opts)
-    vim.treesitter.language.register("markdown", "octo")
-    if LazyVim.has_extra("editor.telescope") then
-      opts.picker = "telescope"
-    elseif LazyVim.has_extra("editor.fzf") then
-      opts.picker = "fzf-lua"
-    elseif LazyVim.has_extra("editor.snacks_picker") then
-      opts.picker = "snacks"
-    else
-      LazyVim.error("`octo.nvim` requires `telescope.nvim` or `fzf-lua` or `snacks.nvim`")
-    end
-
-    -- Keep some empty windows in sessions
-    vim.api.nvim_create_autocmd("ExitPre", {
-      group = vim.api.nvim_create_augroup("octo_exit_pre", { clear = true }),
-      callback = function(ev)
-        local keep = { "octo" }
-        for _, win in ipairs(vim.api.nvim_list_wins()) do
-          local buf = vim.api.nvim_win_get_buf(win)
-          if vim.tbl_contains(keep, vim.bo[buf].filetype) then
-            vim.bo[buf].buftype = "" -- set buftype to empty to keep the window
-          end
-        end
+  cmd = "Octo",
+  opts = {
+    -- or "fzf-lua" or "snacks" or "default"
+    picker = "telescope",
+    -- bare Octo command opens picker of commands
+    enable_builtin = true,
+  },
+  keys = {
+    {
+      "<leader>oi",
+      "<CMD>Octo issue list<CR>",
+      desc = "List GitHub Issues",
+    },
+    {
+      "<leader>op",
+      "<CMD>Octo pr list<CR>",
+      desc = "List GitHub PullRequests",
+    },
+    {
+      "<leader>od",
+      "<CMD>Octo discussion list<CR>",
+      desc = "List GitHub Discussions",
+    },
+    {
+      "<leader>on",
+      "<CMD>Octo notification list<CR>",
+      desc = "List GitHub Notifications",
+    },
+    {
+      "<leader>os",
+      function()
+        require("octo.utils").create_base_search_command({ include_current_repo = true })
       end,
-    })
-  end,
+      desc = "Search GitHub",
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    -- OR "ibhagwan/fzf-lua",
+    -- OR "folke/snacks.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
 }
