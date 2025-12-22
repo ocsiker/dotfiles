07be064405669bc53f3249f04c87e5920913d351
ocsiker <qthang89vn@gmail.com>
2025-12-22 08:10:12 +0700

    feat(sqlcl): config alias for sqlcl

---
+++ home/sqlcl/.sqlcl/login.sql
@@ -0,0 +1,20 @@
+-- --- CONFIG ---
+SET SQLFORMAT ansiconsole
+SET PAGESIZE 1000
+SET LINESIZE 300
+
+-- --- ALIASES ---
+-- Thay vì viết dài dòng, Alias này chỉ gọi file fzf_run.sql ta tạo ở Bước 1
+ALIAS pick=@~/.sqlcl/fzf_run.sql
+
+-- Alias edit nhanh
+ALIAS fzed=HOST ~/.sqlcl/pick_sql.sh
+
+-- Reload
+ALIAS reload=@~/.sqlcl/login.sql
+ALIAS cls=CLEAR SCREEN
+
+-- --- PROMPT (PDB Name) ---
+COLUMN current_pdb NEW_VALUE _my_pdb NOPRINT
+SELECT sys_context('userenv', 'con_name') AS current_pdb FROM dual;
+SET SQLPROMPT "_USER@&_my_pdb > "
