7936ab5307e52f07438c5b4812d3ec542b3a7493
ocsiker <qthang89vn@gmail.com>
2026-01-14 18:33:51 +0700

    feat(sqlcl): increase find file deeper

--- home/sqlcl/.sqlcl/pick_sql.sh
+++ home/sqlcl/.sqlcl/pick_sql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Tìm file sql, loại bỏ lỗi permission, hiển thị fzf đẹp
-TARGET=$(find . -maxdepth 4 -name "*.sql" 2>/dev/null | fzf --preview 'head -20 {}' --height 50% --layout=reverse --border --prompt="SQL> ")
+TARGET=$(find . -maxdepth 6 -name "*.sql" 2>/dev/null | fzf --preview 'head -20 {}' --height 50% --layout=reverse --border --prompt="SQL> ")
 
 # Nếu chọn được file (TARGET không rỗng) thì mở nvim
 if [ -n "$TARGET" ]; then
